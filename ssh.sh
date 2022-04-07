18b01481958acee01c7b884e4a6dfcafed2a5536
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-04-07 17:46:39 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
+if [[ ! "$USER" == "root" ]]; then
+   echo -e "\033[41;33m 警告：请使用root用户操作!~~  \033[0m"
+   exit 1
+fi
+
 function system_check() {
   if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
     system_centos
   elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
-    ssh_PermitRootLogin
-    service ssh restart
+    apt -y update
+    system_ubuntu
   elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
-    ssh_PermitRootLogin
-    service ssh restart
+    apt -y update
+    system_debian
   elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
     system_alpine
   else
-    echo "不支持您的系统"
+    echo -e "\033[41;33m 不支持您的系统  \033[0m"
     exit 1
   fi
 }
 
 function system_centos() {
   if [[ ! -f /etc/ssh/sshd_config ]]; then
+    echo -e "\033[33m 安装SSH \033[0m"
     yum install -y openssh-server
     systemctl enable sshd.service
     ssh_PermitRootLogin
@@ -27,28 +33,62 @@ function system_centos() {
     ssh_PermitRootLogin
     service sshd restart
   fi
+  echo -e "\033[32m 开启root账户SSH完成 \033[0m"
+  exit 0
+}
+
+function system_ubuntu() {
+  if [[ ! -f /etc/ssh/sshd_config ]]; then
+    echo -e "\033[33m 安装SSH \033[0m"
+    apt-get install -y openssh-server
+    ssh_PermitRootLogin
+    service ssh restart
+  else
+    ssh_PermitRootLogin
+    service ssh restart
+  fi
+  echo -e "\033[32m 开启root账户SSH完成 \033[0m"
+  exit 0
+}
+
+function system_debian() {
+  if [[ ! -f /etc/ssh/sshd_config ]]; then
+    echo -e "\033[33m 安装SSH \033[0m"
+    apt install -y openssh-server
+    ssh_PermitRootLogin
+    service ssh restart
+  else
+    ssh_PermitRootLogin
+    service ssh restart
+  fi
+  echo -e "\033[32m 开启root账户SSH完成 \033[0m"
+  exit 0
 }
 
 function system_alpine() {
   if [[ ! -f /etc/ssh/sshd_config ]]; then
-    yum install -y openssh-server
-    systemctl enable sshd.service
+    echo -e "\033[33m 安装SSH \033[0m"
+    apk add openssh-server
+    apk add openssh-client
+    rc-update add sshd
     ssh_PermitRootLogin
     service sshd restart
   else
     ssh_PermitRootLogin
     service sshd restart
   fi
+  echo -e "\033[32m 开启root账户SSH完成 \033[0m"
+  exit 0
 }
 
 function ssh_PermitRootLogin() {
   if [[ `grep -c "ClientAliveInterval 30" /etc/ssh/sshd_config` == '0' ]]; then
     sed -i '/ClientAliveInterval/d' /etc/ssh/sshd_config
     sed -i '/ClientAliveCountMax/d' /etc/ssh/sshd_config
-    sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
     sh -c 'echo ClientAliveInterval 30 >> /etc/ssh/sshd_config'
     sh -c 'echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config'
-    sh -c 'echo PermitRootLogin yes >> /etc/ssh/sshd_config'
   fi
+  sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+  sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 }
 system_check "$@"
