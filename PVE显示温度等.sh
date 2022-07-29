4ab1efcb9d736dda9888e57190dce955df3dd79c
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-07-29 11:12:14 +0800

    Update PVE显示温度等.sh

--- PVE显示温度等.sh
+++ PVE显示温度等.sh
@@ -3,23 +3,43 @@ nodes="/usr/share/perl5/PVE/API2/Nodes.pm"
 pvemanagerlib="/usr/share/pve-manager/js/pvemanagerlib.js"
 proxmoxlib="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
 
+echo 安装lm-sensors
 if ! sensors;then
-  echo 安装lm-sensors
-  apt update && apt-get install lm-sensors && apt-get install nvme-cli && apt-get install hddtemp && chmod +s /usr/sbin/nvme && chmod +s /usr/sbin/hddtemp && chmod +s /usr/sbin/smartctl && systemctl restart pveproxy
-
+  apt update && apt-get install lm-sensors && apt-get install nvme-cli && apt-get install hddtemp && chmod +s /usr/sbin/nvme && chmod +s /usr/sbin/hddtemp && chmod +s /usr/sbin/smartctl
 fi
 
+echo 检测硬件信息
+sensors-detect --auto > /tmp/sensors
+drivers=`sed -n '/Chip drivers/,/\#----cut here/p' /tmp/sensors|sed '/Chip /d'|sed '/cut/d'`
+if [ `echo $drivers|wc -w` = 0 ];then
+    echo 没有找到任何驱动，似乎你的系统不支持。
+else
+    for i in $drivers
+    do
+        modprobe $i
+        if [ `grep $i /etc/modules|wc -l` = 0 ];then
+            echo $i >> /etc/modules
+        fi
+    done
+    sensors
+    sleep 3
+    echo 驱动信息配置成功。
+fi
+rm /tmp/sensors
+
+echo 备份源文件
 pvever=$(pveversion | awk -F"/" '{print $2}')
 echo pve版本$pvever
-echo 备份源文件
 [ ! -e $nodes.$pvever.bak ] && cp $nodes $nodes.$pvever.bak || { echo 已经修改过，请务重复执行; exit 1;}
 [ ! -e $pvemanagerlib.$pvever.bak ] && cp $pvemanagerlib $pvemanagerlib.$pvever.bak
 [ ! -e $proxmoxlib.$pvever.bak ] && cp $proxmoxlib $proxmoxlib.$pvever.bak
 
+# 生成系统变量
 therm='$res->{thermalstate} = `sensors`;';
 cpure='$res->{cpusensors} = `lscpu | grep MHz`;';
 m2temp='$res->{nvme_ssd_temperatures} = `smartctl -a /dev/nvme?|grep -E "Model Number|Total NVM Capacity|Temperature:|Percentage|Data Unit|Power On Hours"`;';
-hddtempe='$res->{hdd_temperatures} = `smartctl -a /dev/sd?|grep -E "Model|Capacity|Power_On_Hours|Temperature"`;';
+hddtempe='$res->{hdd_temperatures} = `smartctl -a /dev/sd?|grep -E "Device Model|Capacity|Power_On_Hours|Temperature"`;';
+
 
 ###################  修改node.pm   ##########################
 echo 修改node.pm：
@@ -27,8 +47,8 @@ sed -i "/PVE::pvecfg::version_text()/a $cpure\n$therm\n$m2temp\n$hddtempe" $node
 # 显示修改结果
 sed -n "/PVE::pvecfg::version_text()/,+5p"  $nodes
 
-###################  修改pvemanagerlib.js   ##########################
 
+###################  修改pvemanagerlib.js   ##########################
 tmpf=tmpfile.temp
 touch $tmpf
 cat > $tmpf << 'EOF'
@@ -39,20 +59,15 @@ cat > $tmpf << 'EOF'
           title: gettext('CPU温度'),
           textField: 'thermalstate',
           renderer:function(value){
-              // CPU包温度
-              // const p0 = value.match(/Package id 0.*?\+([\d\.]+)Â/)[1];
-              const c0 = value.match(/Core 0.*?\+([\d\.]+)Â/)[1];
-              const c1 = value.match(/Core 1.*?\+([\d\.]+)Â/)[1];
-              const c2 = value.match(/Core 2.*?\+([\d\.]+)Â/)[1];
-              const c3 = value.match(/Core 3.*?\+([\d\.]+)Â/)[1];
-              // 主板温度
-              // const b0 = value.match(/temp1.*?\+([\d\.]+)?/)[1];
-              // 主板温度2
-              // const b1 = value.match(/temp2.*?\+([\d\.]+)?/)[1];
-              // 不带主板温度
-              return ` 核心1: ${c0} ℃ | 核心2: ${c1} ℃ | 核心3: ${c2} ℃ | 核心4: ${c3} ℃ `
-              // 带主板温度
-              // return `Package: ${p0} ℃ || 核心1: ${c0} ℃ | 核心2: ${c1} ℃ | 核心3: ${c2} ℃ | 核心4: ${c3} ℃ || 主板: ${b0} ℃ | ${b1} ℃ `
+              // const p0 = value.match(/Package id 0.*?\+([\d\.]+)Â/)[1];  // CPU包温度
+              const c0 = value.match(/Core 0.*?\+([\d\.]+)Â/)[1];  // CPU核心1温度
+              const c1 = value.match(/Core 1.*?\+([\d\.]+)Â/)[1];  // CPU核心2温度
+              const c2 = value.match(/Core 2.*?\+([\d\.]+)Â/)[1];  // CPU核心3温度
+              const c3 = value.match(/Core 3.*?\+([\d\.]+)Â/)[1];  // CPU核心4温度
+              // const b0 = value.match(/temp1.*?\+([\d\.]+)?/)[1];  // 主板温度
+              // const b1 = value.match(/temp2.*?\+([\d\.]+)?/)[1];  // 主板温度2
+              return ` 核心1: ${c0} ℃ | 核心2: ${c1} ℃ | 核心3: ${c2} ℃ | 核心4: ${c3} ℃ `  // 不带主板温度
+              // return `Package: ${p0} ℃ || 核心1: ${c0} ℃ | 核心2: ${c1} ℃ | 核心3: ${c2} ℃ | 核心4: ${c3} ℃ || 主板: ${b0} ℃ | ${b1} ℃ `  // 带主板温度
             }
     },
 	{
@@ -68,6 +83,24 @@ cat > $tmpf << 'EOF'
 			  return `CPU实时: ${f0} MHz | 最小: ${f1} MHz | 最大: ${f2} MHz `
             }
 	},
+
+	// /* 检测不到相关参数的可以注释掉---需要的注释本行即可
+	// 风扇转速
+	{
+          itemId: 'RPM',
+          colspan: 2,
+          printBar: false,
+          title: gettext('CPU风扇'),
+          textField: 'thermalstate',
+          renderer:function(value){
+			  const fan1 = value.match(/fan1:.*?\ ([\d.]+) R/)[1];
+			  const fan2 = value.match(/fan2:.*?\ ([\d.]+) R/)[1];
+			  return `CPU风扇: ${fan1} RPM | 系统风扇: ${fan2} RPM `
+            }
+	},
+	// 检测不到相关参数的可以注释掉---需要的注释本行即可  */
+
+	/* 检测不到相关参数的可以注释掉---需要的注释本行即可
 	// NVME硬盘温度
 	{
           itemId: 'nvme_ssd-temperatures',
@@ -96,6 +129,8 @@ cat > $tmpf << 'EOF'
           }
           }
           },
+	检测不到相关参数的可以注释掉---需要的注释本行即可  */
+
           // SATA硬盘温度
           {
           itemId: 'hdd-temperatures',
@@ -135,8 +170,7 @@ cat > $tmpf << 'EOF'
           },
 EOF
 
-#找到关键字pveversion的行号
-echo 修改pvemanagerlib.js
+echo 找到关键字pveversion的行号
 # 显示匹配的行
 ln=$(sed -n '/pveversion/,+10{/},/{=;q}}' $pvemanagerlib)
 echo "匹配的行号pveversion：" $ln
