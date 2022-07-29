457e9fec2b86d6bb7acce7c19b7a46b70fd04a3e
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-07-29 20:53:38 +0800

    Update J4125开启直通.sh

--- J4125开启直通.sh
+++ J4125开启直通.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 
-
+grep "iommu" /etc/default/grub >/dev/null
+if [ $? -eq 1 ]; then
 sed -i 's/quiet/quiet intel_iommu=on iommu=pt video=efifb:off/g' /etc/default/grub
+fi
 
+
+grep "vfio" /etc/modules >/dev/null
+if [ $? -eq 1 ]; then
 echo vfio >> /etc/modules
 echo vfio_iommu_type1 >> /etc/modules
 echo vfio_pci >> /etc/modules
 echo vfio_virqfd >> /etc/modules
 echo kvmgt >> /etc/modules
+fi
 
-
-echo "options vfio-pci ids=8086:3185" >> /etc/modprobe.d/vfio.conf
+grep "blacklist" /etc/modprobe.d/blacklist.conf >/dev/null
+if [ $? -eq 1 ]; then
 echo "blacklist snd_hda_intel" >> /etc/modprobe.d/blacklist.conf 
 echo "blacklist snd_hda_codec_hdmi" >> /etc/modprobe.d/blacklist.conf 
 echo "blacklist i915" >> /etc/modprobe.d/blacklist.conf 
+fi
 
+grep "vfio" /etc/modprobe.d/vfio.conf >/dev/null
+if [ $? -eq 1 ]; then
+echo "options vfio-pci ids=8086:3185" >> /etc/modprobe.d/vfio.conf
 update-initramfs -u
 update-grub
+fi
+
+
+
+
+
+
