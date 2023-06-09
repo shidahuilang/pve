3a62e78084c59196621afe1eadd36bcfcba05919
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-06-10 00:21:56 +0800

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+创建交换文件
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+永久化交换文件
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+更新 swappiness 值，使系统更倾向于使用物理内存而非交换内存
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+验证交换内存是否正常
+sudo swapon -s
+free -m
