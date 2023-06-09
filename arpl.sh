50a3d283f0c693fdbb3cae2d85cdb138815aa796
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-06-09 20:28:27 +0800

    Update arpl.sh

--- arpl.sh
+++ arpl.sh
@@ -4,7 +4,7 @@
 # 颜色
 red='\033[0;31m'
 green='\033[0;32m'
-yellow='\033[0;33m'
+$yellow='\033[0;33m'
 plain='\033[0m'
 
 # 使用root运行
@@ -12,7 +12,7 @@ plain='\033[0m'
 
 #检查是否是优盘引导
 echo -e "\n $yello正在检查系统...$plain"
-sleep
+sleep 1s
 
 if [ -b /dev/synoboot1 ]; then
     echo -e "$green检测脚本适用于此系统!$plain"
@@ -42,10 +42,10 @@ sleep 1s
 if [ -f /tmp/boot/user-config.yml ]; then
     echo -e "$green检测到user-config.yml文件，正在读取当前序列号..$plain"
     sleep 1s
-    SN=$(grep "set sn" /tmp/boot/user-config.yml |cut -c 8-)
-	MAC=$(grep "set mac" /tmp/boot/user-config.yml |cut -c 8-)
+    SN=$(grep "sn:" /tmp/boot/user-config.yml)
+	MAC=$(grep "mac1:" /tmp/boot/user-config.yml)
     echo -e "$green检测到当前序列号为$SN..$plain"
-	echo -e "$green检测到当前mac为$MAC..$plain"
+	echo -e "$green检测到当前mac1为$MAC..$plain"
 else 
     echo -e "$red没有检测到grub文件！$plain" && exit 1
 fi
@@ -54,14 +54,14 @@ fi
 read -p "请输入新的序列号(按回车键确定)    " NEWSN
 echo -e "$green新序列号为$NEWSN..$plain"
 echo -e "$green正在替换序列号..$plain"
-read -p "请输入新的mac(按回车键确定)    " NEWSN
-echo -e "$green新mac为$NEWMAC..$plain"
-echo -e "$green正在替换mac..$plain"
+read -p "请输入新的mac1(按回车键确定)    " NEWMAC
+echo -e "$green新mac1为$NEWMAC..$plain"
+echo -e "$green正在替换mac1..$plain"
 sleep 1
-sed -i "s/$SN/$NEWSN/g" /tmp/boot/user-config.yml
-sed -i "s/$MAC/$NEWMAC/g" /tmp/boot/user-config.yml
+sed -i "s/$SN/${NEWSN}/g" /tmp/boot/user-config.yml
+sed -i "s/$MAC/${NEWMAC}/g" /tmp/boot/user-config.yml
 if [ $? == 0 ]; then
     echo -e "$green恭喜您，序列号替换成功！请重启系统使配置生效！在控制面板-信息中心查看是否成功。$plain"
 else
-    echo -e "$red序列号替换失败！$plain" && exit 1
+echo -e "$red序列号替换失败！$plain" && exit 1
 fi
